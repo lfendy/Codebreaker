b8c9fb1d151d35f9fa7998d1eec9258cf2f3dc94
Liauw Fendy <lfendy@gmail.com>
2011-11-17 14:02:05 +1100

    First commit with rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codebreaker::Application.config.secret_token = 'd92302db553c212edfd05f6d3f568d754324f1ede5ae359093caa1e90a0b9b4824a6c22d8fecc42a40f0a1c9e82369b0c0a433f65e4af0896ee72d67d0fc3b63'
