d3423c4335df2736aab96345ad16f4a5c255f0d1
Liauw Fendy <lfendy@gmail.com>
2011-11-17 15:38:50 +1100

    completed first iteration, able to view a list of consultants with all attributes

--- spec/views/consultants/index.html.erb_spec.rb
+++ spec/views/consultants/index.html.erb_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 describe "consultants/index.html.erb" do
   it "should display the list of consultants" do
-    assign(:consultants, [stub_model(Consultant, :name=> "Chicago")])
+    assign(:consultants, [stub_model(Consultant, :name=> "Chicago", :email=>"chicago@thoughtworks.com", :role=>"Dev", :grade=>"Consultant", :skill=>"Ruby")])
     render
     rendered.should contain("Chicago")
+    rendered.should contain("chicago@thoughtworks.com")
+    rendered.should contain("Dev")
+    rendered.should contain("Consultant")
+    rendered.should contain("Ruby")
   end
+
 end
