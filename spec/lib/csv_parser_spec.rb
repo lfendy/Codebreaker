7df742118022e954b8ff74c71521da7fb496104d
Liauw Fendy <lfendy@gmail.com>
2011-11-18 12:07:19 +1100

    refactored parsing to return a hash

--- spec/lib/csv_parser_spec.rb
+++ spec/lib/csv_parser_spec.rb
@@ -2,25 +2,27 @@ require 'spec_helper'
 
 describe CSVParser do
   describe 'parse' do
-    it 'should create a Consultant from a single csv line' do
-      consultants = CSVParser.parse 'David Oh,Dev,Consultant,doh@thoughtworks.com,ruby'
-      consultants.first.name.should == 'David Oh'
-      consultants.first.role.should == 'Dev'
-      consultants.first.grade.should == 'Consultant'
-      consultants.first.email.should == 'doh@thoughtworks.com'
-      consultants.first.skill.should == 'ruby'
-      consultants.size.should == 1
+    it 'should parse a file with a single csv line' do
+      results = CSVParser.parse 'David Oh,Dev,Consultant,doh@thoughtworks.com,ruby'
+      results.first.should == {
+        :name => 'David Oh',
+        :role => 'Dev',
+        :grade => 'Consultant',
+        :email => 'doh@thoughtworks.com',
+        :skill => 'ruby'
+      }
+      results.size.should == 1
     end
 
-    it 'should create Consultants from multiple csv lines' do
+    it 'should parse a file with multiple csv lines' do
       str = <<TAG
         David
         Pei Shi
         Weiwei
         Fendy
 TAG
-      consultants = CSVParser.parse str
-      consultants.size.should == 4
+      results = CSVParser.parse str
+      results.size.should == 4
     end
 
   end
