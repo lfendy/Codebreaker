82bff0a37f2918785d8787de4a9546886830c016
Liauw Fendy <lfendy@gmail.com>
2011-11-18 15:40:41 +1100

    fix import csv tests

--- app/controllers/import_csv_controller.rb
+++ app/controllers/import_csv_controller.rb
@@ -2,7 +2,6 @@ require 'csv_parser'
 
 class ImportCsvController < ApplicationController
   def upload
-    
     consultants = CSVParser.parse((params[:dump][:file]).read)
     consultants.each do |c|
       Consultant.create(c)
--- spec/controllers/import_csv_controller_spec.rb
+++ spec/controllers/import_csv_controller_spec.rb
@@ -3,24 +3,31 @@ require 'csv_parser'
 
 describe ImportCsvController do
   describe "upload csv" do
+    before :each do
+      file = ""
+      file.stub(:read).and_return("haha")
+      controller.stub!(:params).and_return({
+        :dump => {:file => file}
+      })
+    end
 
     it 'should parse the csv file' do
       CSVParser.should_receive(:parse).with("haha").and_return([])
-      post 'upload', {:dump => {:file => "haha" }}
+      post 'upload'
     end
 
     it "should create the same number of consultants that was parsed" do
       hashes = [{}, {}]
       CSVParser.stub(:parse).and_return(hashes)
       Consultant.should_receive(:create).twice
-      post 'upload', {:dump => {:file => "haha" }}
+      post 'upload'
     end
 
     it "should create consultants with the parsed attributes" do
       hashes = [{:key=>nil}]
       CSVParser.stub(:parse).and_return(hashes)
       Consultant.should_receive(:create).with(hashes.first)
-      post 'upload', {:dump => {:file => "haha" }}
+      post 'upload'
     end
   end
 
