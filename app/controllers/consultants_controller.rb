05275fd5f3e94356f58590da538b3553b63ffa9f
Liauw Fendy <lfendy@gmail.com>
2011-11-21 17:50:18 +1100

    generate random pair of consultants with skill filtered

--- app/controllers/consultants_controller.rb
+++ app/controllers/consultants_controller.rb
@@ -4,7 +4,25 @@ class ConsultantsController < ApplicationController
   end
 
   def generate
-    @consultants = Consultant.all.sort_by{rand}[0..1]
+    consultants = params[:skill].nil? ?
+      generate_without_filter : generate_with_filter(params[:skill])
+    @consultants = consultants.sort_by{rand}[0..1]
     render :index
+  end
+
+  private
+
+  def generate_with_filter(criteria)
+    skill = Skill.find_by_name(criteria)
+    unless skill.nil?
+      consultants = skill.consultants
+    else
+      consultants = []
+    end
+    consultants
+  end
+
+  def generate_without_filter
+    Consultant.all
   end
 end
--- spec/controllers/consultants_controller_spec.rb
+++ spec/controllers/consultants_controller_spec.rb
@@ -21,18 +21,34 @@ describe ConsultantsController do
 
       get 'generate'
       consultants = assigns[:consultants]
-      consultants.should_not be_nil
       consultants.size.should == 2
+      # why does assigns.should have(2).consultants
     end
 
-    xit 'should filter random consultants based on skills' do
-      (1..5).each do |i|
-        Consultant.create({
-          :name => "David" + i.to_s,
-          :skills_attributes => [{:name => (i<3) ? 'ruby' : 'java'}]
-        })
+    describe "with skill filter" do
+      before :each do
+        (1..5).each do |i|
+          Consultant.create({
+            :name => "David" + i.to_s,
+            :skills_attributes => [{:name => 'ruby'}]
+          })
+        end
       end
-      get 'generate'
+
+      it "should get no consultant if skill doesn't exist" do
+        get 'generate', {:skill => "java"}
+        consultants = assigns[:consultants]
+        consultants.size.should == 0
+      end
+
+      it "should get consultants with the relevant skill" do
+        get 'generate', {:skill => "ruby"}
+        consultants = assigns[:consultants]
+        consultants.size.should == 2
+        consultants.each do |c|
+          c.skills.collect(&:name).should include('ruby')
+        end
+      end
     end
 
   end
